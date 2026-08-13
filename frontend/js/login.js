b678db4b8f5114b5eb93a9ef6812ed4a1408a94e
Arun Kumar Mahto <arunkun07@gmail.com>
2026-08-14 03:45:20 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -17,7 +17,7 @@ async function login() {
 
     try {
 
-        const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        const response = await fetch(`${API_BASE_URL}/auth/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
